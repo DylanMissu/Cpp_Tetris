bf9c48b6e80f5b015b9d20f03c771e90f9219ca2
Dylan Missuwe <dylan.missu@gmail.com>
2020-11-18 11:47:57 +0100

    added method for drawing pixels

--- main.cpp
+++ main.cpp
@@ -7,6 +7,17 @@ using namespace std;
 const int gameWidth = 24;
 const int gameHeight = 12;
 
+string red = "\e[0;31m";
+string RED = "\e[1;31m";
+string blue = "\e[0;34m";
+string BLUE = "\e[1;34m";
+string cyan = "\e[0;36m";
+string CYAN = "\e[1;36m";
+string green = "\e[0;32m";
+string GREEN = "\e[1;32m";
+string yellow = "\e[0;33m";
+string YELLOW = "\e[1;33m";
+
 string buffer[gameWidth][gameHeight];
 
 void DrawGameBorder(int width, int height) 
@@ -15,7 +26,7 @@ void DrawGameBorder(int width, int height)
     {
         for (int j = 0; j < width; j++)
         {
-            if (j == 0 || j == width - 1 || i == 0 || i == height - 1)
+            if (j == 0 || j == width - 1 || i == height - 1)
             {
                 buffer[i][j] = "\xDB\xDB";
             }
@@ -25,6 +36,11 @@ void DrawGameBorder(int width, int height)
             }
         }
     }
+}
+
+void DrawPixel(int x, int y, string colorCode)
+{
+    buffer[x][y] = colorCode + "\xDB\xDB" + "\e[0m";
 }
 
 void DrawBuffer() 
@@ -42,5 +58,6 @@ void DrawBuffer()
 int main()
 {
     DrawGameBorder(12, 24);
+    DrawPixel(5,5, green);
     DrawBuffer();
 }
