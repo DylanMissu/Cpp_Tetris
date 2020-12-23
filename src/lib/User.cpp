3aab7ae3725999fb001bd30daa5b17e9f6037f4b
Dylan Missuwe <dylan.missu@gmail.com>
2020-12-23 16:05:43 +0100

    added user class

---
+++ src/lib/User.cpp
@@ -0,0 +1,11 @@
+#include "User.h"
+
+void User::setUserName(Std::string user)
+{
+    username = user;
+}
+
+std::string User::getUserName()
+{
+    return username;
+}
---
+++ src/lib/User.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+class User
+{
+    public:
+        std::string getUserName()
+        void setUserName(std::string name)
+
+    private:
+        std::string username;
+
+};
